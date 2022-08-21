db5e8b096b820cf25f7bb2f512d72d53afa83ad5
Ayyan Siddiqui <39267085+Ayyan420@users.noreply.github.com>
2022-08-22 03:24:51 +0500

    Adding custom module and database backup

---
+++ custom_language_translation/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Language Translation In Inventory",
+
+    'summary': 'Custom Language Translation In Inventory',
+
+    'description': """
+        Custom Language Translation In Inventory
+    """,
+
+    'author': "Ayyan",
+    'website': "https://github.com/Ayyan420",
+    'category': 'Administration',
+    'version': '13.1.0.1',
+    'installable': True,
+    'license': 'LGPL-3',
+
+    'depends': ['base','stock'],
+
+}
---
+++ custom_language_translation/models/models.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import datetime
+from dateutil import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.osv import expression
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+
+
+# please first run this command on your system in order to work with translation
+# pip install googletrans==3.1.0a0
+
+
+try:
+	from googletrans import Translator
+except ImportError:
+	raise ImportError(
+		'This module needs googletrans to automatically translate texts. '
+		'Please install googletrans on your system. ( sudo pip install googletrans==3.1.0a0 )')
+
+
+
+class warehouse_inherit(models.Model):
+	_inherit = "stock.picking.type"
+
+
+
+
+	def _compute_picking_count(self):
+		res = super(warehouse_inherit, self)._compute_picking_count()
+
+
+		for data in self:
+
+			if data.name:
+
+				 # using googletranslate to translate the name 
+
+				try:
+					translator = Translator()
+					translate_text = translator.translate(data.name,dest='ar',src='auto').text
+
+					# translating to arabic language
+					
+					data.name = translate_text
+
+				except:
+					ValidationError('Network is Slow, Please Fix Your Network Now!')
+
+			else:
+				pass
+
+
+		return res
